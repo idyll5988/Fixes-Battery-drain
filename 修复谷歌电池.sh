7d6c2087d70c0290006dae285b5da760e1f83b11
idyll <liushun968858@gmail.com>
2024-09-27 15:25:06 +0800

    Fixes Battery drain
    Fixes Battery drain, overloading, faultless operation, etc.

---
+++ 修复谷歌电池.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+date="$( date "+%Y年%m月%d日%H时%M分%S秒")"
+function app_list(){
+    pm list packages | grep "$1"
+}
+if [ $(app_list com.google.android.gms) ]; then 
+echo "$date *修复谷歌电池电量耗尽、过载、无故障运行等问题*" 
+su -c "pm disable com.google.android.gms/.chimera.GmsIntentOperationService" 
+fi
