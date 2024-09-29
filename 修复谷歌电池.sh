09adf06c3c6446255d3cd1e02c928484e4b3af79
idyll <liushun968858@gmail.com>
2024-09-29 15:51:26 +0800

    Add files via upload

--- 修复谷歌电池.sh
+++ 修复谷歌电池.sh
@@ -1,5 +1,10 @@
 #!/system/bin/sh
 date="$( date "+%Y年%m月%d日%H时%M分%S秒")"
 echo "$date *修复谷歌电池电量耗尽、过载、无故障运行等问题*" 
-su -c "pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService"
+if pm list packages | grep -q "com.google.android.gms"; then
+    echo "$date *修复谷歌电池电量耗尽、过载、无故障运行等问题*" 
+    su -c "pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService" 
+else
+    echo "$date *谷歌服务未安装，跳过修复*" 
+fi
 
